767382cb55c3d221f5bc96705f989df5b1f61edb
SuellenMMarques <suellenmenezesmarques@gmail.com>
2026-05-07 17:33:25 -0300

    changes html e js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const btnMenu = document.querySelector(".btn-menu");
 const menu = document.querySelector(".list-menu-container");
 const linkMenu = document.querySelectorAll(".link-menu");
-
-linkMenu.forEach(clickedLink);
+let btnGestures = document.querySelectorAll(".btn-gestures");
+let btnChosenUser = "";
+const gestures = ["Pedra", "Papel", "Tesoura"];
+let btnChosenAlexa
+let pontUser = document.querySelector(".pont-user");
+let pontAlexa = document.querySelector(".pont-alexa");
 
 btnMenu.addEventListener("click", function () {
     btnMenu.style.display = "none";
     menu.classList.toggle("active");
 });
+
+linkMenu.forEach(clickedLink);
 
 function clickedLink(item) {
     item.addEventListener("click", function () {
@@ -17,8 +23,47 @@ function clickedLink(item) {
 };
 
 window.addEventListener("click", function (event) {
-    if(menu.contains(event.target) == false && !btnMenu.contains(event.target)){
+    if (menu.contains(event.target) == false && !btnMenu.contains(event.target)) {
         menu.classList.remove("active");
         btnMenu.style.display = "block";
     }
 });
+
+btnGestures.forEach(clickedGesture);
+
+function clickedGesture(item) {
+    item.addEventListener("click", function (event) {
+
+        if (event.target.id == "pedra") {
+            btnChosenUser = "Pedra";
+        } else if (event.target.id == "papel") {
+            btnChosenUser = "Papel";
+        } else {
+            btnChosenUser = "Tesoura";
+        }
+
+        chosenAlexa();
+        checkWinner();
+    });
+};
+
+function chosenAlexa() {
+    let i = Math.floor(Math.random() * 3);
+    btnChosenAlexa = gestures[i];
+    console.log(btnChosenAlexa);
+}
+
+function checkWinner() {
+
+    if (btnChosenUser === btnChosenAlexa) {
+        alert("Empate: Ninguém ganhou!");
+    } else if (btnChosenUser == "Pedra" && btnChosenAlexa == "Tesoura") {
+        alert("Parabéns Você Ganhou!");
+    } else if (btnChosenUser == "Papel" && btnChosenAlexa == "Pedra") {
+        alert("Parabéns Você Ganhou!");
+    } else if (btnChosenUser == "Tesoura" && btnChosenAlexa == "Papel") {
+        alert("Parabéns Você Ganhou!");
+    } else {
+        alert("Que Pena Você Perdeu!");
+    }
+};
